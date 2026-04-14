4bc37671472d9c2767a292f5a8114645a49c9031
Eachan Johnson <eachan.johnson@crick.ac.uk>
2026-04-14 14:13:05 +0100

    Remove vestigial files

--- duvidnn/torch/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from duvida.config import config
-config.set_backend('torch', precision='float')
-
-from .bilinear import TorchBilinearEnsemble
-from .chemprop import ChempropEnsemble
-from .cnn import TorchCNN2DEnsemble
-from .mlp import TorchMLPEnsemble 
